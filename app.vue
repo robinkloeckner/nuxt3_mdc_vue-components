72510b19d258a62e133089e320777c8d368c8d89
Robin Klöckner <mail@robinkloeckner.com>
2025-04-19 21:47:40 +0200

    Initial commit

---
+++ app.vue
@@ -0,0 +1,20 @@
+<script setup>
+const md = `
+# Welcome to docs4.dev
+
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum eum exercitationem harum in iste magni perferendis quo ratione voluptatem voluptatum!
+
+::ad{cssClass="info"}
+<div>Some extra text from the Markdown content.</div>
+::
+
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam at blanditiis commodi doloribus facere necessitatibus nemo quo quos totam vero?
+`
+</script>
+
+<template>
+  <div>
+    <MDC :value="md" />
+  </div>
+</template>
+
---
+++ components/global/Ad.vue
@@ -0,0 +1,33 @@
+<script setup>
+const props = defineProps({cssClass: String});
+</script>
+
+<template>
+  <div class="ad" :class="props.cssClass">
+    <div class="ad-header">Ad: <MDCSlot unwrap="p,div" /></div>
+    <div class="ad-banner">Place you ad here.</div>
+  </div>
+</template>
+
+<style scoped>
+.ad {
+  border: 1px solid #eeeeee;
+  border-radius: 5px;
+  padding: 10px;
+}
+
+.ad-header {
+  font-size: 10px;
+}
+
+.ad-banner {
+  background-color: #eeeeee;
+  border: 1px solid #eeeeee;
+  border-radius: 5px;
+  padding: 10px;
+}
+
+.info {
+  border-color: orange !important;
+}
+</style>
